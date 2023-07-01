1e56ed37142f7a255f6c9ca5416fc69aa78ba197
Lacakali-hu <106344453+Lacakali-hu@users.noreply.github.com>
2023-07-01 15:29:30 +0200

    list and appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,100 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+void main() => runApp(const MyApp());
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    const title = 'Settings';
+
+    return MaterialApp(
+      title: title,
+      theme: ThemeData(
+        fontFamily: 'Cabin',
+        colorSchemeSeed: const Color.fromRGBO(43, 255, 0, 1),
+        useMaterial3: true,
+      ),
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          toolbarHeight: 180,
+          title: const Text(
+            title,
+            style: TextStyle(
+              fontSize: 50,
+              fontFamily: 'Cabin',
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        body: ListView(
+          children: const <Widget>[
+            ListTile(
+              title: const Text(
+                title,
+                style: TextStyle(
+                  fontSize: 50,
+                  fontFamily: 'Cabin',
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            ListTile(
+              title: const Text(
+                title,
+                style: TextStyle(
+                  fontSize: 50,
+                  fontFamily: 'Cabin',
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            ListTile(
+              title: const Text(
+                title,
+                style: TextStyle(
+                  fontSize: 50,
+                  fontFamily: 'Cabin',
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            ListTile(
+              title: const Text(
+                title,
+                style: TextStyle(
+                  fontSize: 50,
+                  fontFamily: 'Cabin',
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            ListTile(
+              title: const Text(
+                title,
+                style: TextStyle(
+                  fontSize: 50,
+                  fontFamily: 'Cabin',
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            ListTile(
+              title: const Text(
+                title,
+                style: TextStyle(
+                  fontSize: 50,
+                  fontFamily: 'Cabin',
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            ListTile(),
+            ListTile(),
+            ListTile(),
+            ListTile(),
+          ],
         ),
       ),
     );
